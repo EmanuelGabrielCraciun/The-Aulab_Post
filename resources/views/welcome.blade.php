91353f0c020db17f0850fa920b9505b3126f89e6
EmanuelGabriel <emanuel.gabriel.craciun@gmail.com>
2024-11-28 14:44:43 +0100

    Scaffolding

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+
+<div class="container-fluid p-5 bg-danger text-center">
+    <div class="row jsutify-content-center">
+        <div class="col-3">
+            <h1>The Aulab Post</h1>
+        </div>
+    </div>
+</div>
+
+</x-layout>
